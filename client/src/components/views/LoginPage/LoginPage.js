340c2d3b8860488626cb47b2c1b5a82db3931a81
gyeolse <threeewave@kakao.com>
2020-12-18 16:27:05 +0900

    [client] React-router Settings

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function LoginPage() {
-  return <div></div>;
+  return <h1>Login Page</h1>;
 }
 
 export default LoginPage;
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function RegisterPage() {
-  return <div></div>;
+  return <h1>Register Page</h1>;
 }
 
 export default RegisterPage;
